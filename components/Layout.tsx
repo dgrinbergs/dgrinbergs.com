3d776d4c82b13c8470cc4dff34b6690f7475859e
dgrinbergs <www.dainis.lv@gmail.com>
2022-03-10 06:14:58 +0000

    page title navigation styling

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import Navigation from "./Navigation";
 import Footer from "./Footer";
 import { ReactNode } from "react";
+import Head from "next/head";
 
 type Props = {
   children: ReactNode;
@@ -9,22 +10,29 @@ type Props = {
 
 const Layout = ({ children, title }: Props) => {
   return (
-    <div className="m-auto dark:bg-neutral-900 dark:text-neutral-300">
-      <div className="min-h-screen">
-        <div className="sticky top-0 z-40 backdrop-blur flex-none transition-colors border-b border-black dark:border-neutral-300">
-          <Navigation />
-        </div>
-        <div className="max-w-7xl m-auto">
-          <div className="mx-4 my-6">
-            {title && <h2 className="text-2xl">{title}</h2>}
-            {children}
+    <>
+      <Head>
+        <title>DGrinbergs.com</title>
+        <meta name="description" content={title} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="m-auto dark:bg-neutral-900 dark:text-neutral-300">
+        <div className="min-h-screen">
+          <div className="sticky top-0 z-40 backdrop-blur flex-none transition-colors border-b border-black dark:border-neutral-300">
+            <Navigation />
+          </div>
+          <div className="max-w-7xl m-auto">
+            <div className="mx-4 my-6">
+              {title && <h2 className="text-2xl">{title}</h2>}
+              {children}
+            </div>
           </div>
         </div>
+        <div className="border-t border-black dark:border-neutral-300">
+          <Footer />
+        </div>
       </div>
-      <div className="border-t border-black dark:border-neutral-300">
-        <Footer />
-      </div>
-    </div>
+    </>
   );
 };
 
--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -2,10 +2,13 @@ import Link from "next/link";
 import { useState } from "react";
 import Hamburger from "./Hamburger";
 import { routes } from "../data/routes";
+import { useRouter } from "next/router";
 
 const Navigation = () => {
   const [open, setOpen] = useState(false);
   const onClick = () => setOpen(!open);
+
+  const { asPath } = useRouter();
 
   return (
     <nav className="flex flex-col justify-between space-y-2 p-4 max-w-7xl m-auto md:flex-row md:items-center md:space-y-0">
@@ -19,20 +22,28 @@ const Navigation = () => {
           <Hamburger open={open} onClick={onClick} />
         </div>
       </div>
-      {open && <ul className={`flex flex-col space-y-4 text-lg`}>
+      {open && (
+        // Mobile links
+        <ul className="flex flex-col space-y-2 text-lg">
+          {routes.map(({ name, href }) => (
+            <li key={name}>
+              <Link href={href}>
+                <a className={`${asPath === href ? "text-sky-500 underline underline-offset-4" : ""}`}>
+                  {name}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+      <ul className="hidden md:flex md:flex-row md:space-y-0 md:items-center md:space-x-4">
         {routes.map(({ name, href }) => (
+          // Desktop links
           <li key={name}>
             <Link href={href}>
-              <a>{name}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>}
-      <ul className={`hidden md:flex md:flex-row md:space-y-0 md:items-center md:space-x-4`}>
-        {routes.map(({ name, href }) => (
-          <li key={name}>
-            <Link href={href}>
-              <a>{name}</a>
+              <a className={`${asPath === href ? "text-sky-500 underline underline-offset-4" : ""} hover:text-sky-500 transition ease-in-out duration-100`}>
+                {name}
+              </a>
             </Link>
           </li>
         ))}
